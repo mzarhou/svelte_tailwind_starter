6e2cf52533d6e97be362689c9104c99898553ccb
mzarhou <mzarhou.cc@gmail.com>
2021-12-12 21:25:34 +0100

    first

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import "../styles/tailwind-output.css"
+</script>
+
+<div class="text-center mt-40">
+	Here we go
+</div>
+<div class="text-center mt-40">
+	Here we go
+</div>
+<div class="text-center mt-40">
+	Here we go
+</div>
+<div class="text-center mt-40">
+	Here we go
+</div>
+
+<style>
+	div {
+		display: inline-block;
+		position: relative;
+		padding: 30px;
+		border: 1px blue solid;
+	}
+	div::after {
+		--size: 10px;
+		content: "";
+		width: var(--size);
+		height: var(--size);
+		border-radius: 50%;
+		background-color: red;
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+	div::before {
+
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{svelte,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
